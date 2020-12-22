8615e79421ea64e37656784eb274c6ae02c26e49
shuymn <mail@shuymn.me>
2020-12-22 10:22:50 +0900

    style: format with prettier

--- src/types/remark-prism.d.ts
+++ src/types/remark-prism.d.ts
@@ -1,5 +1,5 @@
 declare module "remark-prism" {
-  import { Plugin } from "unified"
-  const remarkPrism: Plugin<[]>
-  export = remarkPrism
+  import { Plugin } from "unified";
+  const remarkPrism: Plugin<[]>;
+  export = remarkPrism;
 }

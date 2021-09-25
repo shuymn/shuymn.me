3a93366e3f16896ed030be6f297e3f36d0b87292
Shu Yamani <mail@shuymn.me>
2021-09-25 09:25:59 +0900

    fix: export explicitly

--- src/types/twin.d.ts
+++ src/types/twin.d.ts
@@ -2,5 +2,5 @@ import "twin.macro";
 import styledImport from "styled-components";
 
 declare module "twin.macro" {
-  const styled: typeof styledImport;
+  export const styled: typeof styledImport;
 }

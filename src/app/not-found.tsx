d15ea5f10a5ddc8c51650e744787844b7426c198
shuymn <mail@shuymn.me>
2025-03-20 23:54:00 +0900

    feat: 404エラーページを追加

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <main className="my-12 text-center">
+      <p className="mb-8">
+        お探しのページは移動したか、削除された可能性があります。
+      </p>
+    </main>
+  )
+}

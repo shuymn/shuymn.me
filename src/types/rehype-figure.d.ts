fa9c223151d39e9edebbbb648b1ae881271e86d1
shuymn <mail@shuymn.me>
2025-03-20 17:46:22 +0900

    feat: rehype-figureモジュールの型定義を追加

---
+++ src/types/rehype-figure.d.ts
@@ -0,0 +1,10 @@
+declare module 'rehype-figure' {
+  import type { Plugin, Processor } from 'unified'
+
+  type Options = {
+    className: string
+  }
+
+  const rehypeFigure: Plugin<[Options?] | [Processor?, Options?]>
+  export = rehypeFigure
+}

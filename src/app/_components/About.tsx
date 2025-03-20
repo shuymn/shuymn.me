c1e476720032586161c3e978563558054385fe26
shuymn <mail@shuymn.me>
2025-03-20 17:40:46 +0900

    feat(components): AboutコンポーネントとPostLinkコンポーネントを追加

---
+++ src/app/_components/About.tsx
@@ -0,0 +1,45 @@
+import type React from 'react'
+
+const About: React.FC = () => {
+  return (
+    <section className="mt-8 mb-16">
+      <div className="text-lg">
+        <p>
+          日常的なことやプログラミングに関する個人的な記事を投稿しています。
+        </p>
+        <p>投稿は全て個人の意見であり、所属する組織の意見ではありません。</p>
+        <p>お問い合わせの際は、下記のリンク先からご連絡ください。</p>
+      </div>
+      <ul className="mt-4 flex space-x-2">
+        <li>
+          <a
+            href="https://x.com/shuymn"
+            className="no-underline text-blue-600 visited:text-purple-600 cursor-pointer"
+          >
+            Twitter
+          </a>
+        </li>
+        <li>/</li>
+        <li>
+          <a
+            href="https://github.com/shuymn"
+            className="no-underline text-blue-600 visited:text-purple-600 cursor-pointer"
+          >
+            GitHub
+          </a>
+        </li>
+        <li>/</li>
+        <li>
+          <a
+            href="mailto:mail@shuymn.me"
+            className="no-underline text-blue-600 visited:text-purple-600 cursor-pointer"
+          >
+            mail
+          </a>
+        </li>
+      </ul>
+    </section>
+  )
+}
+
+export default About
---
+++ src/app/_components/PostLink.tsx
@@ -0,0 +1,20 @@
+import type React from 'react'
+import Link from 'next/link'
+
+const PostLink: React.FC<{
+  props: { slug: string; date: string; title: string }
+}> = ({ props: { slug, date, title } }) => {
+  return (
+    <div className="mb-6">
+      <p className="text-sm text-gray-800">{date.replace(/-/g, '.')}</p>
+      <Link
+        href={`/posts/${slug}`}
+        className="text-xl no-underline text-blue-600 visited:text-purple-600 cursor-pointer"
+      >
+        {title}
+      </Link>
+    </div>
+  )
+}
+
+export default PostLink

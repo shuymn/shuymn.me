e0f6da67cdc0b171b01083325613b621051d6f72
shuymn <mail@shuymn.me>
2025-03-20 23:53:56 +0900

    feat: エラーページを追加

---
+++ src/app/error.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+export default function Error() {
+  return (
+    <main className="my-12 text-center">
+      <p className="mb-8">
+        予期せぬエラーが発生しました。しばらく時間をおいてから再度アクセスしてください。
+      </p>
+    </main>
+  )
+}

c83b82f7daa9147a9aae6e5df4540f6d7ead4cd9
shuymn <mail@shuymn.me>
2025-08-03 05:52:05 +0900

    style: apply consistent code formatting with Biome

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,11 +1,9 @@
-'use client'
+"use client";
 
 export default function Error() {
   return (
     <main className="my-12 text-center">
-      <p className="mb-8">
-        予期せぬエラーが発生しました。しばらく時間をおいてから再度アクセスしてください。
-      </p>
+      <p className="mb-8">予期せぬエラーが発生しました。しばらく時間をおいてから再度アクセスしてください。</p>
     </main>
-  )
+  );
 }
--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -1,42 +1,42 @@
-import { readFileSync, readdirSync } from 'node:fs'
-import { join } from 'node:path'
-import matter from 'gray-matter'
+import { readdirSync, readFileSync } from "node:fs";
+import { join } from "node:path";
+import matter from "gray-matter";
 
 export type Post = {
-  slug: string
-  meta: PostMeta
-  content: string
-}
+  slug: string;
+  meta: PostMeta;
+  content: string;
+};
 
 export type PostMeta = {
-  title: string
-  description: string
-  cardImage: string
-  date: string
-}
+  title: string;
+  description: string;
+  cardImage: string;
+  date: string;
+};
 
-const POSTS_DIR = join(process.cwd(), '_posts')
+const POSTS_DIR = join(process.cwd(), "_posts");
 
 export const getPostBySlug = (slug: string): Post => {
-  const realSlug = slug.replace(/\.md$/, '')
-  const fullPath = join(POSTS_DIR, `${realSlug}.md`)
-  const fileContents = readFileSync(fullPath, 'utf8')
-  const { data, content } = matter(fileContents)
+  const realSlug = slug.replace(/\.md$/, "");
+  const fullPath = join(POSTS_DIR, `${realSlug}.md`);
+  const fileContents = readFileSync(fullPath, "utf8");
+  const { data, content } = matter(fileContents);
 
-  return { slug: realSlug, meta: data as PostMeta, content }
-}
+  return { slug: realSlug, meta: data as PostMeta, content };
+};
 
 export const getAllPosts = (): Post[] => {
-  const slugs = readdirSync(POSTS_DIR)
-  return slugs.map((slug) => getPostBySlug(slug))
-}
+  const slugs = readdirSync(POSTS_DIR);
+  return slugs.map((slug) => getPostBySlug(slug));
+};
 
 export const getAllPostsSortByDate = (): Post[] => {
-  const posts = getAllPosts()
+  const posts = getAllPosts();
   return posts.sort((a, b) => {
     if (a.meta.date < b.meta.date) {
-      return 1
+      return 1;
     }
-    return -1
-  })
-}
+    return -1;
+  });
+};
